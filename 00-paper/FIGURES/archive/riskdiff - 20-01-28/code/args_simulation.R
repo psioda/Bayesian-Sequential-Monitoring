b011a6376ff18bb0476eb70b482d4466cb9232f7
kwiatkowski-evan <kwiatkowski-evan@users.noreply.github.com>
2020-01-30 15:06:08 -0500

    deleted archive
    deleted archive

--- 00-paper/FIGURES/archive/riskdiff - 20-01-28/code/args_simulation.R
+++
@@ -1,3 +0,0 @@
-model<-expand.grid(seq(0.39,0.51,by=0.01),.39,2,1,4,seq(0,1,by=0.25))
-names(model)<-c("p.IP","p.PC","freq.mntr","enr.shape","out.mean","mix.prob")
-write.csv(model,file="args_simulation.csv")
