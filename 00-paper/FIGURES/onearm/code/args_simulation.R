96069553614c63fd70fc12f9deab85c3af629700
Kwiatkowski, Evan {MDPA~South San Francisco} <kwiatkowski.evan@gene.com>
2021-07-24 02:07:45 -0400

    updated 1 arm code to monitoring after every 10

--- 00-paper/FIGURES/onearm/code/args_simulation.R
+++ 00-paper/FIGURES/onearm/code/args_simulation.R
@@ -2,19 +2,20 @@ model1 <- expand.grid(1,
                     seq(p.skpt, p.enth, length = 25),
                     c(0, 1),
                     c(0, 1),
-                    2,
+                    10,
                     1,
                     4)
-model2 <- expand.grid(2,
-                    p.skpt,
-                    1,
-                    0,
-                    c(1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60),
-                    c(1, 0.25),
-                    4)
- 
-model <- rbind(model1, model2)
+# model2 <- expand.grid(2,
+#                     p.skpt,
+#                     1,
+#                     0,
+#                     c(1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60),
+#                     c(1, 0.25),
+#                     4)
+#  
+# model <- rbind(model1, model2)
 
+model <- model1
 names(model) <- c("model", 
                 "p.range", 
                 "skpt_spike", 
--- 00-paper/FIGURES/onearm/code/plots/plots_seq_design_prop.R
+++ 00-paper/FIGURES/onearm/code/plots/plots_seq_design_prop.R
@@ -1,21 +1,12 @@
-plot(figure3$p.range,figure3$eff.mon.initial,
-     type='l',ylim=c(0,1),lwd=2,
-     ylab="Probability",xlab="",
-     main=label_main,
-     axes=FALSE)
-box()
+lines(figure3$p.range,
+      figure3$eff.mon.initial,
+     lwd=2)
 
-lines(figure3$p.range,figure3$fut.mon.initial,lwd=2,lty='longdash')
-lines(figure3$p.range,1-figure3$fut.mon.initial-figure3$eff.mon.initial,lwd=2,lty='dotted')
+# lines(figure3$p.range,
+#       figure3$fut.mon.initial,
+#       lwd=2,
+#       lty='longdash')
 
-
-axis(2,las=2,at=seq(0,1,by=0.1),labels=format(seq(0,1,by=0.1),nsmall=1))
-abline(h=seq(0,1,by=0.1),col='grey')
-
-axis(1,las=0,at=figure3_table$p.range[seq(1,length(figure3_table$p.range))],
-     labels=format(figure3_table$p.range[seq(1,length(figure3_table$p.range))],nsmall=2))
-
-mtext(bquote(theta),side=1,line=1,at=stretch)
 row<-2
 for (j in seq(1,length(figure3_table$p.range))){
   mtext(text=paste0(format(round(figure3_table$ss.initial[j],digits=1),nsmall=1),
@@ -26,58 +17,15 @@ for (j in seq(1,length(figure3_table$p.range))){
                     format(round(figure3_table$ss.final[j],digits=1),nsmall=1)),
         side=1,line=row,at=figure3_table$p.range[j])
 }
-mtext(text="SS",side=1,line=row,at=stretch)
-row<-3
-for (j in seq(1,length(figure3_table$p.range))){
-  mtext(text=paste0("(I) ",
-                    format(round(figure3_table$post.mean.initial[j],digits=3),nsmall=3),
-                    " (F) ",
-                    format(round(figure3_table$post.mean.final[j],digits=3),nsmall=3)),
-        side=1,line=row,at=figure3_table$p.range[j])
-}
-mtext(text="PM",side=1,line=row,at=stretch)
-row<-4
-for (j in seq(1,length(figure3_table$p.range))){
-  mtext(text=paste0("(I) ",
-                    format(round(figure3_table$cov.initial[j],digits=3),nsmall=3),
-                    " (F) ",
-                    format(round(figure3_table$cov.final[j],digits=3),nsmall=3)),
-        side=1,line=row,at=figure3_table$p.range[j])
-}
-mtext(text="CP",side=1,line=row,at=stretch)
 
 points(figure3_table$p.range[seq(1,length(figure3_table$p.range))],
        figure3_table$eff.mon.initial[seq(1,length(figure3_table$p.range))]
        ,pch=20)
+mtext(text="SS",side=1,line=row,at=stretch)
 
-text(figure3_table$p.range[seq(1,length(figure3_table$p.range))],
-     figure3_table$eff.mon.initial[seq(1,length(figure3_table$p.range))],
-     labels=format(round(
-       figure3_table$eff.mon.initial[seq(1,length(figure3_table$p.range))],
-       digits=3),nsmall=3),
-     pos=3)
-
-legend("right",#text.width=0.05,
-       legend=c("Stop Early for Efficacy",
-                "Stop Early for Futility"),
-       lty=c('solid','longdash'))
-
-# row<-2
-# for (j in seq(1,length(figure3$p.range),by=9)){
-#   mtext(text=paste0(format(round(figure3$eff.mon.initial[j],digits=3),nsmall=3)),
-#         side=1,line=row,at=figure3$p.range[j])
-# }
-# mtext(text="EFF",side=1,line=row,at=stretch)
-# row<-3
-# for (j in seq(1,length(figure3$p.range),by=9)){
-#   mtext(text=paste0(format(round(figure3$fut.mon.initial[j],digits=3),nsmall=3)),
-#         side=1,line=row,at=figure3$p.range[j])
-# }
-# mtext(text="FUT",side=1,line=row,at=stretch)
-# row<-4
-# for (j in seq(1,length(figure3$p.range),by=9)){
-#   mtext(text=paste0(format(round(1-figure3$fut.mon.initial[j]-figure3$eff.mon.initial[j],
-#                                  digits=3),nsmall=3)),
-#         side=1,line=row,at=figure3$p.range[j])
-# }
-# mtext(text="INC",side=1,line=row,at=stretch)
+# text(figure3_table$p.range[seq(1,length(figure3_table$p.range))],
+#      figure3_table$eff.mon.initial[seq(1,length(figure3_table$p.range))],
+#      labels=format(round(
+#        figure3_table$eff.mon.initial[seq(1,length(figure3_table$p.range))],
+#        digits=3),nsmall=3),
+#      pos=3)
